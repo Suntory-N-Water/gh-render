a495c2bbfaa05208797572c63896bd68f3d6999c
sui <1202ayasnppk@gmail.com>
2026-01-21 22:54:56 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,11 @@
 export default {
 	async fetch(req) {
 		const url = new URL(req.url);
-		url.pathname = '/__scheduled';
-		url.searchParams.append('cron', '* * * * *');
-		return new Response(`To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`);
+		url.pathname = "/__scheduled";
+		url.searchParams.append("cron", "* * * * *");
+		return new Response(
+			`To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`,
+		);
 	},
 
 	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
@@ -30,8 +32,8 @@ export default {
 		// publish to a Queue, query a D1 Database, and much more.
 		//
 		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
+		const resp = await fetch("https://api.cloudflare.com/client/v4/ips");
+		const wasSuccessful = resp.ok ? "success" : "fail";
 
 		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
 		// In this template, we'll just log the result:
