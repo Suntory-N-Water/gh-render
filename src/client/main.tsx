7343e75b9c088b73d67f55772867848408016c4f
sui <1202ayasnppk@gmail.com>
2026-05-10 14:58:13 +0900

    chore: fix knip

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,17 +3,13 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App.tsx';
-import { ThemeProvider } from '@/client/components/theme-provider.tsx';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const rootEl = document.getElementById('root');
 if (!rootEl) {
   throw new Error('#root element not found');
 }
 createRoot(rootEl).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 );
